06fbe4ac2fb60b9ec546c5f7037d9918ca1046ed
Insideher667 <sdsdellasala@gmail.com>
2025-05-24 12:57:33 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongod_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+
 MONGO_CONNECTION_STRING = config_env["MONGODB_CONNECTION_STRING"]
 
 
